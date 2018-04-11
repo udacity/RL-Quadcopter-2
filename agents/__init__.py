01d3704cba6cbbf6bae5f294c4e4659ce15b5930
Dmytro Bagaiev <dmytro.bagaiev@tomra.com>
2018-04-12 00:33:30 +0200

    Don't use demo policy search agent

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,4 +1,4 @@
 from .base_agent import BaseAgent
-from .policy_search import PolicySearch_Agent
+# from .policy_search import PolicySearch_Agent
 from .ddpg_agent import DeepDPGAgent
 from .ddpg_agent import DeepDPGPlayer
