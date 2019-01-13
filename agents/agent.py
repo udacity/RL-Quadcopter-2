63e5a6a8bf877e0ca6aabf62e29bfeebfd7d41b4
chris <christianmoryah@gmail.com>
2019-01-13 10:39:55 -0200

    reach peak results with hyperparam tuning

--- agents/agent.py
+++ agents/agent.py
@@ -28,7 +28,7 @@ class DDPG():
         # Noise process
         self.exploration_mu = 0
         self.exploration_theta = 0.15
-        self.exploration_sigma = 0.2
+        self.exploration_sigma = 0.3
         self.noise = OUNoise(self.action_size, self.exploration_mu, self.exploration_theta, self.exploration_sigma)
 
         # Replay memory
@@ -38,7 +38,7 @@ class DDPG():
 
         # Algorithm parameters
         self.gamma = 0.99  # discount factor
-        self.tau = 0.01  # for soft update of target parameters
+        self.tau = 0.1  # for soft update of target parameters
         
         self.score = 0.
         self.best_score = -np.inf
